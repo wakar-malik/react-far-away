fe3400aae4538610ff6cd2487a7844ad23602669
wakar-malik <wakarmalik9@gmail.com>
2023-10-05 16:23:12 +0530

    completed far-away application

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 function Header() {
   return (
     <div className={styles.headerContainer}>
-      <h1 className={styles.headerTitle}>🏝️ Far Away 🧳<h1>
+      <h1 className={styles.headerTitle}>🏝️ Far Away 🧳</h1>
     </div>
   );
 }
